289e255094b7002014770109549ec45e4ab339f3
Angelika Klimek <angelika@blendcommerce.com>
2022-04-21 08:10:02 +0100

    Put some basic information

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { AppProvider, Page } from '@shopify/polaris';
+import { AppProvider, EmptyState, Page } from '@shopify/polaris';
 
 export default class App extends React.Component {
   render() {
     return (
       <AppProvider>
         <Page>
+        <EmptyState
+          heading="React with Polaris"
+          secondaryAction={{content: 'More about Polaris', url: 'https://polaris.shopify.com/components/get-started'}}
+          image={process.env.PUBLIC_URL + "logo512.png"}
+        >
+        </EmptyState>
         </Page>
       </AppProvider>
     );
